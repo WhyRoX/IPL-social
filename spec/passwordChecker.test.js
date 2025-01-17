1619437f7614f1132095dc93a25e131e397ed465
WhyRoX <enes.12.yumer@gmail.com>
2025-01-17 17:07:18 +0100

    fix: small fix for testing functions

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -58,11 +58,11 @@ describe("hasDigit", () => {
 
 describe("notContainsIPL", () => {
     it('should return false for passwords containing "IPL" (case insensitive)', () => {
-        expect(isValidPassword("aB1!IPLxyz")).toBe(false);
-        expect(isValidPassword("aB1!iplxyz")).toBe(false);
+        expect(notContainsIPL("aB1!IPLxyz")).toBe(false);
+        expect(notContainsIPL("aB1!iplxyz")).toBe(false);
     });
     
     it('should return true for passwords not containing "IPL" (case insensitive)', () => {
-        expect(isValidPassword("aB1!5165xyz")).toBe(true);
+        expect(notContainsIPL("aB1!5165xyz")).toBe(true);
     });
 });
