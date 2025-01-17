2e85394aa5dd1b0b86769ce7b9398ec66c3ceb49
WhyRoX <enes.12.yumer@gmail.com>
2025-01-17 16:20:26 +0100

    test: initial specialCharacter test

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -4,7 +4,12 @@ describe('Password Validation', () => {
     it('should return false for passwords shorter than 8 characters', () => {
         expect(isValidPassword('1234567')).toBe(false);
     });
+
     it('should return true for password longer or equal to 8 characters', () => {
         expect(isValidPassword('12345678')).toBe(true);
+    });
+
+    it('should return false for passwords without a special character', () => {
+        expect(isValidPassword('abcdhzitjn0')).toBe(false);
     });
 });
